8111e489887532cc26b5aaa2fda6865b330ba88e
Abdullah AlBannay <aabannay@gmail.com>
2018-04-01 17:27:08 +0300

    add the winning modal

--- js/app.js
+++ js/app.js
@@ -59,9 +59,11 @@ function clickCard(event){
 		let numOfOpenCards = addToOpenCards(event);
 		if (numOfOpenCards == 2){
 			checkMatch();
+			countMoves();
+			updateStars();
+			checkAllMatched();
 		}
-		countMoves();
-		updateStars();
+
 	}
 }
 
@@ -111,3 +113,44 @@ function updateStars(){
 		stars.removeChild(stars.firstElementChild);
 	} else {;}
 }
+function checkAllMatched() {
+	let matchCounter = document.querySelectorAll('.match').length;
+	if(matchCounter == 16){
+		//then all matched.. display winning message.
+		fireWinPopup();
+	}
+}
+
+const myModal = document.querySelector('#myModal');
+function fireWinPopup() {
+    modal.style.display = "block";
+}
+
+
+// Modal code
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "block";//"none";
+    }
+}
+
