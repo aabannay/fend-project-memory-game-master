dd52d657c3e41e5c8fe7db168b0afdee40fcb25b
Abdullah AlBannay <aabannay@gmail.com>
2018-04-01 17:34:06 +0300

    disable modal window closure

--- js/app.js
+++ js/app.js
@@ -127,30 +127,25 @@ function fireWinPopup() {
 }
 
 
-// Modal code
+// Modal code from W3Schools:
+// https://www.w3schools.com/howto/howto_css_modals.asp
 // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-//var btn = document.getElementById("myBtn");
+const modal = document.getElementById('myModal');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, DONT close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "block";//"none";
+        modal.style.display = "block"; //if want to enable change to: "none";
     }
 }
+
+const playAgainButton = document.getElementById('replay-button');
+
+playAgainButton.onclick = function(event) {
+
+}
+
 
